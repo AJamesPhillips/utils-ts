0877997adae551a0bda8f197856702e62b87779b
Alexander James Phillips <ajp.global@gmail.com>
2017-03-20 17:07:20 +0000

    Add tests

---
+++ src/json.ts
@@ -0,0 +1,17 @@
+
+
+// Given a date or string, return a date
+// Given an optional date or optional string, return an optional date
+export function from_pojo_date(value: Date | string): Date
+export function from_pojo_date(value: Date | string | undefined): Date | undefined
+export function from_pojo_date(value: Date): Date {
+  return value && new Date(value.toString());
+}
+
+// Given a boolean or string, return a boolean
+// Given an optional boolean or optional string, return an optional boolean
+export function from_pojo_bool(value: boolean | string): boolean
+export function from_pojo_bool(value: boolean | string | undefined): boolean | undefined
+export function from_pojo_bool(value: boolean): boolean {
+  return value && (value.toString() === 'true');
+}
